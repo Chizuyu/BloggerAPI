79fd282cbab2f89bf7716908b8397afcfc1583e5
Yazid <hazamiyazid7@gmail.com>
2026-05-26 08:51:00 +0700

    feat: add apidbcontext for database access

---
+++ BloggerAPI/Data/ApiDbContext.cs
@@ -0,0 +1,14 @@
+using BloggerAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BloggerAPI.Data
+{
+    public class ApiDbContext : DbContext
+    {
+        public ApiDbContext(DbContextOptions<ApiDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
