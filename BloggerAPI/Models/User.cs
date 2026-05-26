1e6f30fe5cd4c5234651551c7e6293b8c86b3fb6
Yazid <hazamiyazid7@gmail.com>
2026-05-26 10:38:26 +0700

    feat: add User entity model based on schema

---
+++ BloggerAPI/Models/User.cs
@@ -0,0 +1,14 @@
+namespace BloggerAPI.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string? FirstName{ get; set; }
+        public string? LastName { get; set; }
+        public string Username { get; set; } = string.Empty;
+        public string PasswordHash { get; set; } = string.Empty; // Di DB namanya PasswordHash
+        public DateTime? DateOfBirth { get; set; }
+        public DateTime JoinDate { get; set; } = DateTime.UtcNow;
+        public string? Photo { get; set; }
+    }
+}
