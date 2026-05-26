b5c5e2aaaff70f5afcf30b99f67f5bb96f9fe15c
Yazid <hazamiyazid7@gmail.com>
2026-05-26 16:35:35 +0700

    feat: Update Post and Comment entities with Guid relations

--- BloggerAPI/Entities/Comment.cs
+++ BloggerAPI/Entities/Comment.cs
@@ -1,8 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace BloggerAPI.Entities
 {
     public class Comment
     {
+        [Key]
         public Guid Id { get; set; } = Guid.NewGuid();
+        [Required]
         public string Content { get; set; } = string.Empty;
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 
--- BloggerAPI/Entities/Post.cs
+++ BloggerAPI/Entities/Post.cs
@@ -1,18 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace BloggerAPI.Entities
 {
     public class Post
     {
-        public Guid Id { get; set; }
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+        [Required]
         public string Title { get; set; } = string.Empty;
+        [Required]
         public string Content { get; set; } = string.Empty;
         public string? Thumbnail { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
 
         // Relations
         public Guid CategoryId { get; set; }
         public Category? Category { get; set; }
         public Guid UserId { get; set; }
         public User? User { get; set; }
-        public ICollection<Comment> Comments { get; set; } = [];
+
+        public ICollection<Comment> Comments { get; set; } = new List<Comment>();
     }
 }
